1c315ee29d288020715515aebab87a01ef4d6240
baconrepo <45722492+baconrepo@users.noreply.github.com>
2020-01-31 11:53:20 -0500

    Update hw3B.py

--- hw3B.py
+++ hw3B.py
@@ -6,31 +6,37 @@ Method:
 3.
 
 """
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+
 
 def vin(t):
-    if:  even return 1
-    else: its odd return -1
+    if (math.floor(2*t) % 2 ==0):  ## rounds down to nearest whole, if even
+        return 1
+    else:
+        return -1     ##if odd
 
-def function_to_int(RC,vout):  ###(a,b,h)
+def function_to_int(RC,vout,t):  ###(a,b,h)
     return (1/RC)*(vin(t)-vout)
 
 
 
-integrate function(a,b,RC, )
-    def RKfour(a,b,RC,y,n,vin,vout):
-        x_n=[0]   ### creates list to populate with initial value=0
+def filter_fun(a,b,RC,y,n ):   ##uses RK 4th order to integrate
+    #def RKfour(a,b,RC,y,n,vin,vout):
+        vout_n=[0]   ### creates list to populate with initial value=0
         t_n=[a]  ##first value in t list is initial value to start from (independent variable)
         h=(b-a)/n    ##evenly spaced step size
         while t_n[-1] < b:   ##iterates from initial_t a->b
-            k_1= h*function(RC,x_n[-1],t_n[-1])
-            k_2= h*function(RC,x_n[-1] + k_1/2, t_n[-1] + h/2)
-            k_3= h*function(RC,x_n[-1] + k_2/2, t_n[-1] + h/2)
-            k_4= h*function(RC,x_n[-1] + k_3, t_n[-1] +h)
+            k_1= h*function_to_int(RC,vout_n[-1],t_n[-1])
+            k_2= h*function_to_int(RC,vout_n[-1] + k_1/2, t_n[-1] + h/2)
+            k_3= h*function_to_int(RC,vout_n[-1] + k_2/2, t_n[-1] + h/2)
+            k_4= h*function_to_int(RC,vout_n[-1] + k_3, t_n[-1] + h)
             #y=x_n[0] + h*function(x_n[-1],t_n[-1])     ##calculate the next approximate value in the function (then append to list in a later line)
             t_n.append(t_n[-1]+h)    ##add a value to the t list with the latest value plus the slice width, stepping
-            x_n.append(x_n[-1] + (k_1 + 2*k_2 + 2*k_3 + k_4)/6)        ##append the list with the calculated value
-        plt.plot(t_n,x_n)
+            vout_n.append(vout_n[-1] + (k_1 + 2*k_2 + 2*k_3 + k_4)/6)        ##append the list with the calculated value
+        plt.plot(t_n,vout_n)
         plt.title("R-K 4th order")
         plt.show()
-    RKfour(0,10,0.01,function,50)
-    plt.show()
+filter_fun(0,10,.001,function_to_int,50000)
+plt.show()
